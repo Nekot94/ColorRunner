1669f569bc1d54290b1a47a2fe6ee20282ef0540
Nekot94 <nekomato9@gmail.com>
2017-02-27 16:56:05 +0300

    3rd unner

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public static GameController instance;
+
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text bestScoreText;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject loseText;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        GetBestScore();
+        loseText.SetActive(false);
+    }
+
+    public void EndGame() {
+        loseText.SetActive(true);
+        StartCoroutine(RestartLevel(2f));
+    }
+    private IEnumerator RestartLevel(float time) {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("Main");
+    }
+    public void ChangeScore(int score) {
+        SetBestScore(score);
+        scoreText.text = "Очки:" + score.ToString();
+    }
+
+    private void SetBestScore(int score) {
+        if (!PlayerPrefs.HasKey("Score") || PlayerPrefs.GetInt("Score") < score) {
+            PlayerPrefs.SetInt("Score", score);
+            bestScoreText.text = "HighScore:" + score.ToString();
+        }
+    }
+    private void GetBestScore() {
+        if (PlayerPrefs.HasKey("Score")) {
+            int score = PlayerPrefs.GetInt("Score");
+            bestScoreText.text = "HighScore:" + score.ToString();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,19 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 15f;
 
+    private int score;
+
     private Rigidbody rb;
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private AudioSource playerTalk;
+    [SerializeField]
+    private AudioSource playerCollect;
+    [SerializeField]
+    private AudioClip collectSound;
+    [SerializeField]
+    private AudioClip deadSound;
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
 		
 	}
@@ -18,4 +28,32 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3 (speed * Time.deltaTime, 0, 0);
         rb.AddForce(movement);
 	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Box") {
+            score++;
+            speed += 10f;
+            GameController.instance.ChangeScore(score);
+            Die (collision);
+        }
+    }   
+
+    void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag == "Box") {
+            Die(collision);
+        }
+    }
+    private void Die(Collision collision) {
+        BoxStates colState = collision.gameObject.GetComponent<BoxScript> ().State;
+        if(colState == BoxStates.red) {
+            collision.gameObject.SetActive(false);
+            GameController.instance.EndGame();
+            if(playerTalk != null && deadSound != null) {
+                playerCollect.clip = deadSound;
+                playerCollect.Play();
+            }
+        }
+            
+
+    }
 }
